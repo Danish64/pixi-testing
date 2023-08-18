434a451facf0a4e0ba33fa31781b83c185f603c8
Danish <danishali0051@gmail.com>
2023-08-19 03:00:45 +0500

    added accesibility test

--- pixi-test/src/Pixi/Pixi.jsx
+++ pixi-test/src/Pixi/Pixi.jsx
@@ -4,6 +4,9 @@ import { BlurFilter } from "pixi.js";
 import '@pixi/accessibility';
 
 const bunny = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png";
+
+const isE2E = navigator.webdriver;
+
 export const Pixi = () => {
 
   // without this line, all mouse events are broken
@@ -11,23 +14,24 @@ export const Pixi = () => {
 
   const [spriteScale, setSpriteScale] = useState({ x: 2, y: 2 });
 
+  const onClick = () => {
+    setSpriteScale((spriteScale) => ({x: spriteScale.x * 5, y: spriteScale.y * 5}))
+  }
+
   return (
-  <Stage width={1920} height={1080} options={{ backgroundColor: 0x012b30 }}>
+  <Stage width={1920} height={1080} options={{ backgroundColor: 0x012b30 }} data-testid="canvas">
     <Sprite
       x={250}
       y={250}
       anchor={[0.5, 0.5]}
-      interactive={true}
-      accessible={true}
-      accessibleTitle="I am bunny, hehe"
+      interactive
+      accessible={isE2E}
+      accessibleTitle="I am bunny"
+      debug={isE2E}
       scale={spriteScale}
       image={bunny}
-      pointerdown={() => {
-        setSpriteScale((spriteScale) => ({x: spriteScale.x * 5, y: spriteScale.y * 5}))
-      }}
-      ontap={() => {
-        setSpriteScale((spriteScale) => ({x: spriteScale.x * 1.25, y: spriteScale.y * 1.25}))
-      }}
+      pointerdown={onClick}
+      ontap={onClick}
     />
   </Stage>
 )};
--- tests/pixi.spec.js
+++ tests/pixi.spec.js
@@ -10,11 +10,21 @@ test.use({
 test('pixi test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(2000);
 
   await expect(page).toHaveTitle("Pixi Testing");
 
-  page.mouse.click(250, 250);
+  const canvas = page.locator("data-testid=canvas");
+
+  await expect(canvas).toBeVisible();
+
+  page.keyboard.press("Tab");
+
+  const bunny = page.getByTitle("I am bunny");
+
+  await expect(bunny).toBeVisible();
+
+  await bunny.click();
 
   await expect(page).toHaveScreenshot("pixi.png");
 });
